16c94290994a73ee4477a5e6dfe056b090723673
Julien Stefani <julien.stefani@soprasteria.com>
2018-11-07 10:24:25 +0100

    Refactor Ionic template with my name pages

--- src/pages/community/community.ts
+++ src/pages/community/community.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-community',
+  templateUrl: 'community.html'
 })
-export class ContactPage {
+export class CommunityPage {
 
   constructor(public navCtrl: NavController) {
 
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { CommunityPage } from '../community/community';
+import { ProfilePage } from '../profile/profile';
+import { MapPage } from '../map/map';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = MapPage;
+  tab2Root = ProfilePage;
+  tab3Root = CommunityPage;
 
   constructor() {
 
