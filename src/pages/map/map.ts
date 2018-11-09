02902e0d72341d8c225ec717371bd74e4c6ec9a2
Julien Stefani <julien.stefani@soprasteria.com>
2018-11-09 11:24:24 +0100

    Added a locate button to the map. Fixed multi-markers

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController, Content } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 import leaflet from 'leaflet';
+import { ToastController } from 'ionic-angular';
 
 import { SearchPlacePage } from './searchPlace';
 
@@ -19,30 +20,51 @@ export class MapPage {
   pushPage: any;
 
   private searchVisible: boolean = false;
+  private myPositionMarker: any;
 
-  constructor(public navCtrl: NavController, private geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, private geolocation: Geolocation, 
+    public toastCtrl: ToastController) {
     this.pushPage = SearchPlacePage;
   }
 
-  ionViewDidEnter() {
-    this.loadmap();
+  ionViewDidLoad() {
+    this.loadmap().then(() => {
+      this.getCurrentGeolocation();
+    });
   }
 
   loadmap() {
-    this.map = leaflet.map("map").fitWorld();
-    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attributions: 'www.tphangout.com',
-      maxZoom: 18
-    }).addTo(this.map);
+    console.log('LOAD MAP');
+    return new Promise((resolve) => {
+      this.map = leaflet.map("map").fitWorld();
+      leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attributions: 'www.tphangout.com',
+        maxZoom: 18
+      }).addTo(this.map);
+      resolve();
+    });
+  }
+
+  getCurrentGeolocation() {
+    let toast = this.toastCtrl.create({
+      message: 'Localisation en cours... Veuillez patienter.',
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.present(toast);
     this.geolocation.getCurrentPosition().then((resp) => {
+      if (this.myPositionMarker !== undefined) {
+        this.myPositionMarker.remove();
+      }
       this.map.setView([resp.coords.latitude, resp.coords.longitude], 14);
-      let markerGroup = leaflet.featureGroup();
+      this.myPositionMarker = leaflet.featureGroup();
       let marker: any = leaflet.marker([resp.coords.latitude, resp.coords.longitude]).on('click', () => {
         alert('Marker clicked');
-      })
-      markerGroup.addLayer(marker);
-      this.map.addLayer(markerGroup);
       });
+      this.myPositionMarker.addLayer(marker);
+      this.map.addLayer(this.myPositionMarker);
+    });
   }
 
 }
